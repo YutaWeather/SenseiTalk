1c9b8402537dfe2d5b1b7a5eca87d8729f526094
Yuta Fujii <yutayutakid21@gmail.com>
2022-01-26 13:52:09 +0900

    ヘッダーメニュー追加実装

--- SenseiTalk/Controller/PageViewController.swift
+++ SenseiTalk/Controller/PageViewController.swift
@@ -6,50 +6,44 @@
 //
 
 import UIKit
+import TabPageViewController
 
-class PageViewController: UIPageViewController,UIPageViewControllerDataSource {
-    
+//class PageViewController: UIPageViewController,UIPageViewControllerDataSource {
+class PageViewController: TabPageViewController {
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    override init() {
+        super.init()
         let timelineVC1 = STTimeLineVC()
         timelineVC1.pageNum = 0
+        timelineVC1.view.tag = 0
+        let timelineVC2 = STTimeLineVC()
+        timelineVC2.pageNum = 1
+        timelineVC2.view.tag = 1
+        let timelineVC3 = STTimeLineVC()
+        timelineVC3.pageNum = 2
+        timelineVC3.view.tag = 2
         
-        self.setViewControllers([timelineVC1], direction: .forward, animated: true, completion: nil)
-        self.dataSource = self
-        
+
+        tabItems = [(timelineVC1, "小学校"), (timelineVC2, "中学校"),(timelineVC3, "高校")]
+        option.tabWidth = view.frame.width / CGFloat(tabItems.count)
+        option.hidesTopViewOnSwipeType = .all
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 5, height: 10))
+        imageView.contentMode = .scaleAspectFit
+        let image = UIImage(named: "avatar-placeholder")
+        imageView.image = image
+        self.navigationItem.titleView = imageView
     }
     
-    //    func getFirst() -> STTimeLineVC {
-    //        let timelineVC1 = STTimeLineVC()
-    //        timelineVC1.pageNum = 0
-    //        return timelineVC1
-    //    }
-    //
-    //    func getSecond() -> STTimeLineVC {
-    //        let timelineVC2 = STTimeLineVC()
-    //        timelineVC2.pageNum = 1
-    //        return timelineVC2
-    //    }
-    //
-    //    func getThird() -> STTimeLineVC {
-    //        let timelineVC3 = STTimeLineVC()
-    //        timelineVC3.pageNum = 2
-    //        return timelineVC3
-    //    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        
-        let vc:STTimeLineVC = STTimeLineVC()
-        vc.pageNum = (viewController as! STTimeLineVC).pageNum - 1
-        return vc
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        let vc:STTimeLineVC = STTimeLineVC() // コード生成したりStoryboardから取ったり
-        vc.pageNum = (viewController as! STTimeLineVC).pageNum + 1
-        return vc
-        
-    }
-    
+
     
 }
